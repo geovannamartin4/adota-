f6924b7364fcd4e9518aa06d32df8f41795f1f86
Geovanna <geovannamartin4@gmail.com>
2026-08-20 21:40:49 -0300

    Adiciona backend Spring Boot

---
+++ backend/src/test/java/com/adota/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adota.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
